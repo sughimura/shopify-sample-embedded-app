59f202c6275f5a9966f92a7bfc3169288e0fd3a4
sughimura <nxsughi@gmail.com>
2019-09-26 19:09:23 +0900

    feat: 'Polaris UIを作成' の手前まで

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,11 @@
-import {AppProvider, EmptyState, Layout, Page} from '@shopify/polaris';
-
-const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+import { TextStyle } from '@shopify/polaris';
 
 const Index = () => (
-  <AppProvider>
-    <Page>
-      <EmptyState
-        heading="Discount your products temporarily"
-        action={{
-          content: 'Select products',
-          onAction: () => console.log('clicked'),
-        }}
-        image={img}
-      >
-        <p>Select products to change their price temporarily.</p>
-      </EmptyState>
-    </Page>
-  </AppProvider>
+  <div>
+    <TextStyle variation="positive">
+      Sample app using React and Next.js
+    </TextStyle>
+  </div>
 );
 
 export default Index;
