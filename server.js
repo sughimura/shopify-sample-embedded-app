cb37d2917b268d0f30e68e6da0a2c43307799ab9
sughimura <nxsughi@gmail.com>
2019-09-27 16:39:51 +0900

    feat: GraphQLを設定

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
 
 dotenv.config();
+const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -14,6 +15,7 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
+const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 
 app.prepare().then(() => {
   const server = new Koa();
@@ -33,6 +35,7 @@ app.prepare().then(() => {
     }),
   );
 
+  server.use(graphQLProxy({version: ApiVersion.April19}))
   server.use(verifyRequest());
   server.use(async (ctx) => {
     await handle(ctx.req, ctx.res);
